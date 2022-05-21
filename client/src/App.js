b4288b839dd2b90875d568c2a8843630141614a1
Kyle Deato <kyledeato@gmail.com>
2022-05-21 01:14:29 -0700

    add hashrouter instead of browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Main from './views/Main';
 import Create from './views/Create';
 import View from './views/View';
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className="App">
       
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/pirates" element={<Main/>}></Route>
+          <Route path="/" element={<Main/>}></Route>
           <Route path="/pirates/new" element={<Create/> } ></Route>
           <Route path="/pirate/:id" element={<View/> } ></Route> 
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
